6f7ec34100dd8aaafe9a97f613d66bc2d54d0825
Victor Abuka <87471426+vickerdent@users.noreply.github.com>
2023-05-24 22:22:27 +0100

    Make additional changes

---
+++ memory.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * bfree - Frees the pointer and then NULLs the address
+ * @ptr: serves as address of pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+if (ptr && *ptr)
+{
+free(*ptr);
+*ptr = NULL;
+return (1);
+}
+return (0);
+}
---
+++ realloc.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ * _memset - Fills the memory with a constant byte
+ * @s: Pointer to memory area
+ * @b: Byte to fill *s with
+ * @n: Amount of bytes that have to be filled
+ * Return: (s) a pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
+}
+
+/**
+ * ffree – serves to free a string of strings
+ * @pp: serves as string of strings
+ */
+void ffree(char **pp)
+{
+char **a = pp;
+
+if (!pp)
+return;
+while (*pp)
+free(*pp++);
+free(a);
+}
+
+/**
+ * _realloc - Reallocates the block of memory
+ * @ptr: Pointer to the previous malloc allocated block
+ * @old_size: Byte size of the previous block
+ * @new_size: Byte size of the new block
+ *
+ * Return: Pointer to the old block.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *p;
+
+if (!ptr)
+return (malloc(new_size));
+if (!new_size)
+return (free(ptr), NULL);
+if (new_size == old_size)
+return (ptr);
+
+p = malloc(new_size);
+if (!p)
+return (NULL);
+
+old_size = old_size < new_size ? old_size : new_size;
+while (old_size--)
+p[old_size] = ((char *)ptr)[old_size];
+free(ptr);
+return (p);
+}
